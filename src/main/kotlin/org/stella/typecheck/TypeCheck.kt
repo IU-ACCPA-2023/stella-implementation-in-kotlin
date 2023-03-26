1214728e907bea553cdbd396ee056ed5bdf5aa23
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2023-03-26 06:34:17 +0300

    Add sample code using when in typechecking module

--- src/main/kotlin/org/stella/typecheck/TypeCheck.kt
+++ src/main/kotlin/org/stella/typecheck/TypeCheck.kt
@@ -1,11 +1,24 @@
 package org.stella.typecheck
 
+import org.syntax.stella.Absyn.AProgram
+import org.syntax.stella.Absyn.DeclFun
+import org.syntax.stella.Absyn.DeclTypeAlias
 import org.syntax.stella.Absyn.Program
 
 object TypeCheck {
     @Throws(Exception::class)
     fun typecheckProgram(program: Program) {
-        println("typechecker is not implemented!")
-        return
+        when (program) {
+            is AProgram -> program.listdecl_.map {
+                when (it) {
+                    is DeclFun -> {
+                        val name = it.stellaident_
+                        println("Declared function $name")
+                    }
+                    is DeclTypeAlias -> {}
+                }
+            }
+
+        }
     }
 }
