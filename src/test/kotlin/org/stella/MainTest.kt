3c0fc39e112f9d34dd8fbe29137aaf63cea6912c
i.nafikov <i.nafikov@tinkoff.ru>
2023-04-12 23:38:15 +0300

    feat: feat: make test all files from each of directories

--- src/test/kotlin/org/stella/MainTest.kt
+++ src/test/kotlin/org/stella/MainTest.kt
@@ -1,72 +1,59 @@
 package org.stella
 
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
-import java.io.File
+import org.junit.jupiter.params.provider.MethodSource
 import java.io.FileInputStream
 import java.io.IOException
-
-const val baseDir = "src/test/resources"
-const val wellTypedDir = "$baseDir/well-typed"
-const val illTypedDir = "$baseDir/ill-typed"
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.stream.Stream
 
 internal class MainTest {
     @ParameterizedTest(name = "{index} Typechecking well-typed program {0}")
-    @ValueSource(strings = [
-        "$wellTypedDir/factorial.stella",
-        "$wellTypedDir/squares.stella",
-        "$wellTypedDir/higher-order-1.stella",
-        "$wellTypedDir/increment_twice.stella",
-        "$wellTypedDir/logical-operators.stella"
-    ])
+    @MethodSource("wellTypedPathStream")
     @Throws(
         IOException::class,
         Exception::class
     )
-    fun testWellTyped(filepath: String) {
+    fun testWellTyped(filepath: Path) {
+        println(filepath.toUri())
         val original = System.`in`
-        val fips = FileInputStream(File(filepath))
+        val fips = FileInputStream(filepath.toFile())
         System.setIn(fips)
         main()
         System.setIn(original)
     }
 
     @ParameterizedTest(name = "{index} Typechecking ill-typed program {0}")
-    @ValueSource(strings = [
-        "$illTypedDir/applying-non-function-1.stella",
-        "$illTypedDir/applying-non-function-2.stella",
-        "$illTypedDir/applying-non-function-3.stella",
-        "$illTypedDir/argument-type-mismatch-1.stella",
-        "$illTypedDir/argument-type-mismatch-2.stella",
-        "$illTypedDir/argument-type-mismatch-3.stella",
-        "$illTypedDir/bad-if-1.stella",
-        "$illTypedDir/bad-if-2.stella",
-        "$illTypedDir/bad-succ-1.stella",
-        "$illTypedDir/bad-succ-2.stella",
-        "$illTypedDir/bad-succ-3.stella",
-        "$illTypedDir/shadowed-variable-1.stella",
-        "$illTypedDir/undefined-variable-1.stella",
-        "$illTypedDir/undefined-variable-2.stella",
-        "$illTypedDir/bad-squares-1.stella",
-        "$illTypedDir/bad-squares-2.stella"
-    ])
+    @MethodSource("illTypedPathStream")
     @Throws(
         IOException::class,
         Exception::class
     )
-    fun testIllTyped(filepath: String) {
+    fun testIllTyped(filepath: Path) {
+        println(filepath.toUri())
         val original = System.`in`
-        val fips = FileInputStream(File(filepath))
+        val fips = FileInputStream(filepath.toFile())
         System.setIn(fips)
-        var typecheckerFailed = false
-        try {
+        assertThrows<Exception>("expected the typechecker to fail!") {
             main()
-        } catch (e: java.lang.Exception) {
-            typecheckerFailed = true
         }
-        if (!typecheckerFailed) {
-            throw java.lang.Exception("expected the typechecker to fail!")
-        }        // TODO: check that there is a type error actually, and not a problem with implementation
         System.setIn(original)
+    }
+
+    companion object {
+        private const val BASE_DIR = "src/test/resources"
+        private const val WELL_TYPED_DIR = "$BASE_DIR/well-typed"
+        private const val ILL_TYPED_DIR = "$BASE_DIR/ill-typed"
+
+        @JvmStatic
+        fun wellTypedPathStream(): Stream<Path> = getFilesStream(WELL_TYPED_DIR)
+
+        @JvmStatic
+        fun illTypedPathStream(): Stream<Path> = getFilesStream(ILL_TYPED_DIR)
+
+        private fun getFilesStream(path: String) = Files.list(Paths.get(path))
     }
 }
